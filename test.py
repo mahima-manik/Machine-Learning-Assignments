f4b21e96eddb2e76ab9bc7638556d18217982959
Mahima Manik <30531520+mahima-manik@users.noreply.github.com>
2018-01-24 12:04:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,42 +7,49 @@ import time
 def cost_function(theta, x, y):
     j_theta = 0
     for i in range(0, len(x)):
-        j_theta += (y[i]-(theta[0]+theta[1]*x[i]))**2
+        j_theta+= (y[i]-(theta[0]+theta[1]*x[i]))**2
     j_theta /= 2
     return j_theta
 
 def summation (y, theta, x, k):
     res = 0
-    for i in range(0, len(x)): 
+    for i in range(0, len(x)):
         if k!=0:
-            res += ((y[i]-(theta[0]+theta[1]*x[i])))*x[i]
-        else: 
-            res += ((y[i]-(theta[0]+theta[1]*x[i])))
+            res += ((y[i]-(theta[0]+theta[1]*x[i]))*x[i])
+        else:
+            res += (y[i]-(theta[0]+theta[1]*x[i]))
     return res
 
 def batch_gd(x, y, eta):
-    theta_array = []
-    theta_array.append([0, 0])      #initializing with 0
+    theta0 = []
+    theta1 = []
+    theta0.append(0)
+    theta1.append(0)
     while True:
-        theta_array.append([0, 0])
+        theta0.append(0)
+        theta1.append(0)
         for i in range(0, 2):
-            theta_array[len(theta_array)-1][i] = theta_array[len(theta_array)-2][i] + eta * summation(y, theta_array[len(theta_array)-2], x, i)/len(x)
+            if i==0:
+                theta0[len(theta0)-1] = theta0[len(theta0)-2] + eta * summation(y,[theta0[len(theta0)-2], theta1[len(theta1)-2]], x, i)
+            else:
+                theta1[len(theta1)-1] = theta1[len(theta1)-2] + eta * summation(y, [theta0[len(theta0)-2], theta1[len(theta1)-2]], x, i)
         
-        a1 = cost_function(theta_array[len(theta_array)-1], x, y)
-        a2 = cost_function(theta_array[len(theta_array)-2], x, y)
-        print theta_array[len(theta_array)-1], a1
-        #print theta_array[len(theta_array)-2], a2
-        #time.sleep(0.05)
-        
+        a1 = cost_function([theta0[len(theta0)-1], theta1[len(theta1)-1]], x, y)
+        a2 = cost_function([theta0[len(theta0)-2], theta1[len(theta1)-2]], x, y)
+        print len(theta0), theta0[len(theta0)-1], theta1[len(theta1)-1], a1
+                
         if (a1-a2 >= 0):
-            print a1
-            return theta_array[len(theta_array)-1]
+            '''cost2d = []
+            for i in range(0, len(theta0)):
+                new = []
+                for j in range(0, len(theta1)):
+                    new.append(cost_function([theta0[i], theta1[i]], x, y))
+                cost2d.append(new)'''
+            return theta0, theta1
 
 if __name__ == "__main__":
     fx = open("linearX.csv","r")
     fy = open("linearY.csv", "r")
-
-    theta = [0, 0]  #[theta0, theta1]
     x = []
     y = []
     for line in fx:
@@ -50,20 +57,15 @@ if __name__ == "__main__":
 
     for line in fy:
         y.append(float(line))
-
-    cost_function(theta, x, y)
-    t = batch_gd(x, y, 0.01)
-    plt.scatter(x, y)
+    
+    theta0, theta1 = batch_gd(x,y,0.0001)
+    
     predicted = []
     for i in range(0, len(x)):
-        predicted.append(t[0]+t[1]*x[i])
-
-    print t[0], t[1]
-    X=np.array(x)
-    print "Ys and predicted"
-    '''for i in range(0,len(y)):
-        for j in range(0, len(predicted)):
-            if i==j:
-                #print y[i], predicted[j]'''
-    plt.plot(X, (t[0]+t[1]*X))
+        predicted.append(theta0[len(theta0)-1]+theta1[len(theta1)-1]*x[i])
+    
+    print theta0[len(theta0)-1], theta1[len(theta1)-1]
+    
+    plt.scatter(x, y)
+    plt.plot(x, predicted)
     plt.show()
